04b481f6637ca281bd5e4b186f5ae3e08fb44e59
Meg Grasse <mgrasse@apple.com>
2020-12-07 19:30:45 -0600

    Initial Commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by Meg Grasse on 12/7/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
