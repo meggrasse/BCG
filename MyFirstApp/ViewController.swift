8feed02df9d310067cef15cc9dbfc59e55443af3
Meg Grasse <mgrasse@apple.com>
2021-01-04 09:51:54 -0600

    Week 2: Part 2: Loops

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -11,13 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        self.colorRed()
+
+        self.loop()
     }
     
-    func colorRed() {
-        // RGB is normalized on iOS.
-        self.view.backgroundColor = UIColor(red: 255 / 255, green: 0, blue: 0, alpha: 1)
+    func loop() {
+        // (1) show for loop syntax in swift and different range operators [... vs ..<],
+        // using print, so students can discern the difference.
+        for i in 0..<10 {
+            print(i)
+        }
+        
+        // (2) show how to use variables in Swift.
+        // `var` - show i can be assigned to a mutable var.
+        // `let` - variable's value can never change. Show how the compiler enforces this.
+        // Pause for 5 minutes to let students play on their own and get comfortable with the loop syntax.
+        var index = 0
+        for i in 0..<10 {
+            index = i
+            print(index)
+        }
+        
+        // (3) Introduce the `stride` function, so students can count by
+        // decimal increments and  count down.
+        for i in stride(from: 1, to: 0, by: -0.001) {
+            print(i)
+        }
     }
 }
 
